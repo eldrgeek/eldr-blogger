b577dfdbb83680339bd05475c0aad2d1e0c23291
Glitch (eldr-blogger) <none>
2018-06-11 15:58:30 +0000

    Refactored and put in update sheets logic

--- server.js
+++ server.js
@@ -15,9 +15,10 @@ let dataDeets; //data to pull from spreadsheet
 const clientID = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const blogId = process.env.BLOG_KEY;
+const sheetId = process.env.SHEET_KEY;
 
 var callbackURL = 'https://'+process.env.PROJECT_DOMAIN+'.glitch.me/login/google/return';
-var scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly',
+var scopes = ['https://www.googleapis.com/auth/spreadsheets',
               'https://www.googleapis.com/auth/plus.me',
               'https://www.googleapis.com/auth/blogger'
              ];
@@ -59,9 +60,12 @@ app.get('/logoff',
 );
 
 app.get('/auth/google', function(req, res) {
-  
-  console.log("redirecting to ", url);
-  res.redirect(url);
+  if(req.cookies.googleauth) {
+    res.redirect("/success");
+  }else {
+    console.log("redirecting to ", url);
+    res.redirect(url);
+  }
 });
 
 app.get('/login/google/return', function(req, res) {
@@ -72,14 +76,12 @@ app.get('/login/google/return', function(req, res) {
         console.log("Setting credentials")
         res.cookie('access_token', tokens.access_token)
         res.cookie('refresh_token', tokens.refresh_token)
-        let keys = Object.keys(tokens)
-        console.log("keys are", keys)
         oauth2Client.setCredentials({
           access_token: tokens.access_token,
           refresh_token: tokens.refresh_token
-          
         });
-        res.redirect('/setcookie');
+        res.cookie('googleauth', new Date());
+        res.redirect('/success');
       } else {
         console.log("Error on getToken: " + err);
       }
@@ -87,20 +89,10 @@ app.get('/login/google/return', function(req, res) {
   }
 );
 
-// on successful auth, a cookie is set before redirecting
-// to the success view
-app.get('/setcookie',
-  function(req, res) {
-    console.log("Going to set the cookie");
-    res.cookie('googleauth', new Date());
-    res.redirect('/success');
-  }
-);
 
 // if cookie exists, success. otherwise, user is redirected to index
 app.get('/success',
   function(req, res) {
-    console.log("success setting cookie");
     if(req.cookies['googleauth']) {
       oauth2Client.setCredentials({
           access_token: req.cookies.access_token,
@@ -115,25 +107,43 @@ app.get('/success',
   }
 );
 
-app.get('/getData',
+app.get('/getStatus',
   function(req, res) {
+  }
+);
+
+const pushSheet = () => {
+  let request = {
+          sheetId: sheetId,
+          range: "A1",
+          auth: oauth2Client,
+// fields=etag%2Citems%2Ckind%2CnextPageToken&key={YOUR_API_KEY} 
+        
+       requestBody: {
+         "values": [
+            [
+             "ValueA1",
+             "ValueB1"
+            ],
+            [
+             "Row2A1",
+             "Row2B1"
+            ]
+         ],
+         "majorDimension": "ROWS"
+                 }
+       }
+sheets.spreadsheets.values.update(request, (err, response) =>
+    {
+      if( err) console.log(err.errors);
+    })
+}
+
+
+const getData =  (req, res) => {
     // Get Google+ details
     console.log("get data")
-    // plus.people.get({
-    //   userId: 'me',
-    //   auth: oauth2Client
-    // }, function (err, response) {
-    //   console.log("Returned from getting people")
-    //   if (err) {
-    //     console.log("Failed getting people: " + err);
-    //     res.send("Failed to get from the people");
-    //   } else { 
-    //     console.log("got people");
-    //     if(response.data.isPlusUser==true){
-    //       userName = response.data.name.givenName;
-    //     } else {
-    //       userName = "Unknown Stranger";        
-    //     }
+  
        let blogRequest = {
            blogId: blogId,
            fetchBodies:false,
@@ -142,18 +152,72 @@ app.get('/getData',
             auth: oauth2Client
 // fields=etag%2Citems%2Ckind%2CnextPageToken&key={YOUR_API_KEY} 
         }
+       let postsData = [];
        const getAndPrintPosts = () => {
          blogger.posts.list(blogRequest, function(err, response) {
           console.log("getting posts");
           if(err) {console.log("error", err); return;}
           printPosts(response);
-          return
           if( response.data.nextPageToken ) {
-          getAndPrintPosts()
+          //getAndPrintPosts(postsData)
+          // } else {
+              console.log("postsData", postsData)
+              res.send(postsData)
+            }
+          })
+        }
+
+       
+       
+       let postNo = 0
+       const printPosts = (response) => {
+         let items = response.data.items;
+          let count = items.length;
+          let item = items[0]
+          console.log( item.title, item.url, item.labels )
+          //patchIt(item.id, item.title, item)
+   
+          for( let i = 0; i< items.length; i++ ){
+            let item = items[i]
+            console.log(++postNo, item.title)
+            postsData.push([item.title, item.url,item.labels])
           }
-         })
+          let nextToken = response.data.nextPageToken;
+          blogRequest.pageToken = nextToken;
        }
-       const patchIt = (id,title,item) => {
+       
+       getAndPrintPosts() 
+       
+        // Now get spreadsheet values
+        // var request = {
+        //   // The ID of the spreadsheet to retrieve data from.
+        //   spreadsheetId: process.env.SHEET_KEY,
+        //   // The A1 notation of the values to retrieve.
+        //   range: 'A1:I8', 
+        //   auth: oauth2Client
+        // };
+        // sheets.spreadsheets.values.update(request, function(err, response) {
+        //   debugger
+        //   if (err) {
+        //     console.log("Aww, man: " + err);
+        //     res.send("An error occurred");
+        //   } else {
+        //     console.log(response.data.values)
+        //     dataDeets = response.data.values;
+        //     res.send([userName, dataDeets]);
+        //     console.log(userName, dataDeets)
+        //   }
+        // });
+    //   }
+    // });
+  }
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+const patchIt = (id,title,item) => {
          console.log("Patchit")
          console.log("title", title, "labels", item.labels)
          if (!item.labels) item.labels = []
@@ -179,50 +243,21 @@ app.get('/getData',
           }
         })
        }
-       let postNo = 0
-       const printPosts = (response) => {
-         let items = response.data.items;
-          let count = items.length;
-          let item = items[0]
-          console.log( item.title, item.url, item.labels )
-          patchIt(item.id, item.title, item)
-         return;
-          for( let i = 0; i< items.length; i++ ){
-            console.log(++postNo, items[i].title)
-          }
-          let nextToken = response.data.nextPageToken;
-          blogRequest.pageToken = nextToken;
-       }
-       
-       getAndPrintPosts() 
-       res.send(["1", "2", "3"])
-
-        // Now get spreadsheet values
-        // var request = {
-        //   // The ID of the spreadsheet to retrieve data from.
-        //   spreadsheetId: process.env.SHEET_KEY,
-        //   // The A1 notation of the values to retrieve.
-        //   range: 'A1:I8', 
-        //   auth: oauth2Client
-        // };
-        // sheets.spreadsheets.values.get(request, function(err, response) {
-        //   debugger
-        //   if (err) {
-        //     console.log("Aww, man: " + err);
-        //     res.send("An error occurred");
-        //   } else {
-        //     console.log(response.data.values)
-        //     dataDeets = response.data.values;
-        //     res.send([userName, dataDeets]);
-        //     console.log(userName, dataDeets)
-        //   }
-        // });
-    //   }
-    // });
+const getPeople = () =>{
+    // plus.people.get({
+    //   userId: 'me',
+    //   auth: oauth2Client
+    // }, function (err, response) {
+    //   console.log("Returned from getting people")
+    //   if (err) {
+    //     console.log("Failed getting people: " + err);
+    //     res.send("Failed to get from the people");
+    //   } else { 
+    //     console.log("got people");
+    //     if(response.data.isPlusUser==true){
+    //       userName = response.data.name.givenName;
+    //     } else {
+    //       userName = "Unknown Stranger";        
+    //     }
   }
-);
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.get('/getData', getData)
